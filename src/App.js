10d9ee2b8e605156ada3ab20439ebceb65e7d638
Tino Mewes <t.mewes@deinetuer.de>
2018-07-05 07:42:36 +0200

    code cleanup: removed core ui sample components

--- src/App.js
+++ src/App.js
@@ -20,13 +20,11 @@ import "codemirror/addon/lint/lint.css";
 import "codemirror/addon/hint/show-hint.css";
 import "cypher-codemirror/dist/cypher-codemirror-syntax.css";
 
-import SimpleBar from 'simplebar';
-import "simplebar/dist/simplebar.css";
+//import SimpleBar from 'simplebar';
+import "simplebar/dist/simplebar.css"; //TODO: is this really neccesary?
 
 // Containers
 import { DefaultLayout } from './containers';
-// Pages
-import { Login, Page404, Page500, Register } from './views/Pages';
 
 // import { renderRoutes } from 'react-router-config';
 
@@ -35,10 +33,6 @@ class App extends Component {
     return (
       <HashRouter>
         <Switch>
-          <Route exact path="/login" name="Login Page" component={Login} />
-          <Route exact path="/register" name="Register Page" component={Register} />
-          <Route exact path="/404" name="Page 404" component={Page404} />
-          <Route exact path="/500" name="Page 500" component={Page500} />
           <Route path="/" name="Home" component={DefaultLayout} />
         </Switch>
       </HashRouter>
