f4ea0ab6cc35202a34ed3c8b9c674965dfc6f5ca
Tino Mewes <t.mewes@deinetuer.de>
2018-07-22 09:28:33 +0200

    added knowledge distribution test

---
+++ src/tests/views/Dashboard/Architecture/FileTypes/FileTypes.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ArchitectureFileTypes from "../../../../../views/Dashboard/Architecture/FileTypes/FileTypes";
+import { shallow } from "enzyme/build/index";
+
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+    ReactDOM.render(<ArchitectureFileTypes />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it("click on popover", () => {
+    var wrapper = shallow(<ArchitectureFileTypes />);
+    wrapper.find("#Popover1").simulate("click");
+});
